36819bacc861a0beb046a5ff8a7c15e0f79bed7a
gabewolford <gabewolford@gmail.com>
2024-02-07 11:22:25 -0800

    updated letter spacing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: "variable",
+  display: "swap",
+  style: "normal",
+});
 
 export const metadata = {
   title: "Outdoor Recreation Archive",
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} tracking-tight`}>{children}</body>
     </html>
   );
 }
