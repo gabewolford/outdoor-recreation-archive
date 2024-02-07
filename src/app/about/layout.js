85f73e1762cd607d9a72b566a044a5be82808364
gabewolford <gabewolford@gmail.com>
2024-02-07 15:22:22 -0800

    added header and titles to page layouts

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,9 +1,11 @@
 import BreadcrumbsWithTitle from "../components/BreadcrumbsWithTitle";
+import TheHeader from "../components/TheHeader";
 
 export default function Layout({ children }) {
   return (
     <main className="p-3 text-sm text-gray-main">
-      <BreadcrumbsWithTitle />
+      <TheHeader />
+      <BreadcrumbsWithTitle title="Introduction" />
       {children}
     </main>
   );
