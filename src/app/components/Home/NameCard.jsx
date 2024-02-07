d0080300f9676532ec2c247bf529a40fb6d5166b
gabewolford <gabewolford@gmail.com>
2024-02-07 11:22:54 -0800

    increased padding, added year for mobile

--- src/app/components/Home/NameCard.jsx
+++ src/app/components/Home/NameCard.jsx
@@ -1,6 +1,6 @@
 export default function NameCard() {
   return (
-    <div className="col-span-1 row-span-1 p-2 text-2xl relative">
+    <div className="flex flex-col justify-between col-span-1 row-span-1 p-3 text-2xl relative">
       {/* Bottom-left corner */}
       <div className="hidden lg:block absolute bottom-0 left-0 w-4 h-px bg-crosshairs-gray transform -translate-x-1/2 translate-y-1/2"></div>
       <div className="hidden lg:block absolute bottom-0 left-0 w-px h-4 bg-crosshairs-gray transform -translate-x-1/2 translate-y-1/2"></div>
@@ -8,13 +8,14 @@ export default function NameCard() {
       <div className="block lg:hidden absolute bottom-0 right-0 w-4 h-px bg-crosshairs-gray transform translate-x-1/2 translate-y-1/2"></div>
       <div className="block lg:hidden absolute bottom-0 right-0 w-px h-4 bg-crosshairs-gray transform translate-x-1/2 translate-y-1/2"></div>
       {/* Content */}
-      <h1 className="text-6xl lg:text-3xl leading-tight lg:leading-snug">
+      <h1 className="text-5xl lg:text-3xl">
         Outdoor
         <br />
         Recreation
         <br />
         Archive
       </h1>
+      <h6 className="lg:hidden text-2xl">1970-2024</h6>
     </div>
   );
 }
