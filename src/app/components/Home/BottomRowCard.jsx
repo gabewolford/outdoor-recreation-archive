979452767aba0c1ef041a12e559fae96a4d95929
gabewolford <gabewolford@gmail.com>
2024-02-08 14:02:18 -0800

    added auto image carousel and placeholders

--- src/app/components/Home/BottomRowCard.jsx
+++ src/app/components/Home/BottomRowCard.jsx
@@ -1,10 +1,41 @@
-import Link from "next/link";
+"use client";
 
-export default function BottomRowCard({ title, linkTo }) {
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+export default function BottomRowCard({ title, linkTo, imageArray }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isHovered) {
+      interval = setInterval(() => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === imageArray.length - 1 ? 0 : prevIndex + 1
+        );
+      }, 200); // Change image every .5 seconds (adjust as needed)
+    }
+
+    return () => clearInterval(interval);
+  }, [isHovered, imageArray]);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <Link
       href={linkTo}
-      className="hidden lg:block col-span-1 p-3 text-lg relative hover:bg-blue-main hover:text-white"
+      className="hidden lg:block col-span-1 text-lg relative overflow-hidden hover:text-white"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       {/* Top-right corner */}
       <div className="absolute top-0 right-0 w-4 h-px bg-gray-main transform translate-x-1/2 -translate-y-1/2"></div>
@@ -13,7 +44,25 @@ export default function BottomRowCard({ title, linkTo }) {
       <div className="absolute bottom-0 right-0 w-4 h-px bg-gray-main transform translate-x-1/2 translate-y-1/2"></div>
       <div className="absolute bottom-0 right-0 w-px h-4 bg-gray-main transform translate-x-1/2 translate-y-1/2"></div>
       {/* Content */}
-      {title}
+      <div className="relative overflow-hidden h-full">
+        {isHovered && imageArray.length > 0 && (
+          <div className="absolute top-0 left-0 right-0 bottom-0">
+            <Image
+              src={imageArray[currentIndex]}
+              alt={`Catalog ${currentIndex + 1}`}
+              fill
+              objectFit="cover"
+            />
+          </div>
+        )}
+        <h3
+          className={`${
+            isHovered ? "hidden" : "flex"
+          } absolute p-3 top-0 left-0 right-0`}
+        >
+          {title}
+        </h3>
+      </div>
     </Link>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,24 +2,84 @@ import BottomRowCard from "./components/Home/BottomRowCard";
 import CornerCrosshairs from "./components/Home/CornerCrosshairs";
 import NameCard from "./components/Home/NameCard";
 import TopRowCard from "./components/Home/TopRowCard";
+import photo1 from "../../public/images/catalog-covers/abercrombie.jpeg";
+import photo2 from "../../public/images/catalog-covers/alpenlite.jpeg";
+import photo3 from "../../public/images/catalog-covers/alpine-outfitters.jpeg";
+import photo4 from "../../public/images/catalog-covers/alti-wear.jpeg";
+import photo5 from "../../public/images/catalog-covers/ascente.jpeg";
+import photo6 from "../../public/images/catalog-covers/asolo.jpeg";
+import photo7 from "../../public/images/catalog-covers/cabelas.jpeg";
+import photo8 from "../../public/images/catalog-covers/california-mountaineering.jpeg";
+import photo9 from "../../public/images/catalog-covers/camelbak.jpeg";
+import photo10 from "../../public/images/catalog-covers/camelbak.jpeg";
+import photo11 from "../../public/images/catalog-covers/cannondale.jpeg";
+import photo12 from "../../public/images/catalog-covers/canoe-specialists.jpeg";
+import photo13 from "../../public/images/catalog-covers/carhartt.jpeg";
+import photo14 from "../../public/images/catalog-covers/cascade-designs.jpeg";
+import photo15 from "../../public/images/catalog-covers/chaco.jpeg";
+import photo16 from "../../public/images/catalog-covers/cmi.jpeg";
+
+const catalogCovers = [
+  photo1,
+  photo2,
+  photo3,
+  photo4,
+  photo5,
+  photo6,
+  photo7,
+  photo8,
+  photo9,
+  photo10,
+  photo11,
+  photo12,
+  photo13,
+  photo14,
+  photo15,
+  photo16,
+];
 
 export default function HomePage() {
   return (
     <main className="h-svh max-h-svh lg:grid lg:grid-cols-5 lg:grid-rows-2 overflow-hidden relative">
       <CornerCrosshairs />
       <NameCard />
-      <TopRowCard title="About" linkTo="/about" />
-      <TopRowCard title="Books" linkTo="/books" />
-      <TopRowCard title="Catalogs" linkTo="/catalogs" />
-      <TopRowCard title="Magazines" linkTo="/magazines" />
-      <BottomRowCard title="Manuscripts" linkTo="/manuscripts" />
-      <BottomRowCard title="Periodicals" linkTo="/periodicals" />
-      <BottomRowCard title="Photographs" linkTo="/photographs" />
+      <TopRowCard title="About" linkTo="/about" imageArray={catalogCovers} />
+      <TopRowCard title="Books" linkTo="/books" imageArray={catalogCovers} />
+      <TopRowCard
+        title="Catalogs"
+        linkTo="/catalogs"
+        imageArray={catalogCovers}
+      />
+      <TopRowCard
+        title="Magazines"
+        linkTo="/magazines"
+        imageArray={catalogCovers}
+      />
+      <BottomRowCard
+        title="Manuscripts"
+        linkTo="/manuscripts"
+        imageArray={catalogCovers}
+      />
+      <BottomRowCard
+        title="Periodicals"
+        linkTo="/periodicals"
+        imageArray={catalogCovers}
+      />
+      <BottomRowCard
+        title="Photographs"
+        linkTo="/photographs"
+        imageArray={catalogCovers}
+      />
       <BottomRowCard
         title="Research Fellowship"
         linkTo="/research-fellowship"
+        imageArray={catalogCovers}
       />
-      <BottomRowCard title="Donate" linkTo="/donate" />
+      <BottomRowCard
+        title="Donate"
+        linkTo="/donate"
+        imageArray={catalogCovers}
+      />
     </main>
   );
 }
