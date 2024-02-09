3d96feb3be2daff78e0493257fc43dfee9b5496f
gabewolford <gabewolford@gmail.com>
2024-02-08 21:36:55 -0800

    images sorted by color, mapped to home cards

--- src/app/page.js
+++ src/app/page.js
@@ -2,40 +2,302 @@ import BottomRowCard from "./components/Home/BottomRowCard";
 import CornerCrosshairs from "./components/Home/CornerCrosshairs";
 import NameCard from "./components/Home/NameCard";
 import TopRowCard from "./components/Home/TopRowCard";
-import photo1 from "../../public/images/catalog-covers/abercrombie.jpeg";
-import photo2 from "../../public/images/catalog-covers/alpenlite.jpeg";
-import photo3 from "../../public/images/catalog-covers/alpine-outfitters.jpeg";
-import photo4 from "../../public/images/catalog-covers/alti-wear.jpeg";
-import photo5 from "../../public/images/catalog-covers/ascente.jpeg";
-import photo6 from "../../public/images/catalog-covers/asolo.jpeg";
-import photo7 from "../../public/images/catalog-covers/cabelas.jpeg";
-import photo8 from "../../public/images/catalog-covers/california-mountaineering.jpeg";
-import photo9 from "../../public/images/catalog-covers/camelbak.jpeg";
-import photo10 from "../../public/images/catalog-covers/camelbak.jpeg";
-import photo11 from "../../public/images/catalog-covers/cannondale.jpeg";
-import photo12 from "../../public/images/catalog-covers/canoe-specialists.jpeg";
-import photo13 from "../../public/images/catalog-covers/carhartt.jpeg";
-import photo14 from "../../public/images/catalog-covers/cascade-designs.jpeg";
-import photo15 from "../../public/images/catalog-covers/chaco.jpeg";
-import photo16 from "../../public/images/catalog-covers/cmi.jpeg";
+import red1 from "../../public/images/catalog-covers/red/alaska.jpg";
+import red2 from "../../public/images/catalog-covers/red/alaska2.jpg";
+import red3 from "../../public/images/catalog-covers/red/altra.jpg";
+import red4 from "../../public/images/catalog-covers/red/alpine-aid.jpg";
+import red5 from "../../public/images/catalog-covers/red/arcteryx.jpg";
+import red6 from "../../public/images/catalog-covers/red/atomic.jpg";
+import red7 from "../../public/images/catalog-covers/red/big-agnes.jpg";
+import red8 from "../../public/images/catalog-covers/red/black-ice.jpg";
+import red9 from "../../public/images/catalog-covers/red/cmi.jpg";
+import red10 from "../../public/images/catalog-covers/red/fabiano.jpg";
+import red11 from "../../public/images/catalog-covers/red/frostline.jpg";
+import red12 from "../../public/images/catalog-covers/red/marmot.jpg";
+import red13 from "../../public/images/catalog-covers/red/montbell.jpg";
+import red14 from "../../public/images/catalog-covers/red/sierra-designs.jpg";
+import red15 from "../../public/images/catalog-covers/red/sierra-designs2.jpg";
+import orange1 from "../../public/images/catalog-covers/orange/black-diamond.jpg";
+import orange2 from "../../public/images/catalog-covers/orange/camping-equipment.jpg";
+import orange3 from "../../public/images/catalog-covers/orange/coleman.jpg";
+import orange4 from "../../public/images/catalog-covers/orange/cotopaxi.jpg";
+import orange5 from "../../public/images/catalog-covers/orange/early-winters.jpg";
+import orange6 from "../../public/images/catalog-covers/orange/fairydown.jpg";
+import orange7 from "../../public/images/catalog-covers/orange/go-lite.jpg";
+import orange8 from "../../public/images/catalog-covers/orange/marmot.jpg";
+import orange9 from "../../public/images/catalog-covers/orange/moonstone.jpg";
+import orange10 from "../../public/images/catalog-covers/orange/nemo-2.jpg";
+import orange11 from "../../public/images/catalog-covers/orange/outdoor-supply-co.jpg";
+import orange12 from "../../public/images/catalog-covers/orange/parajumpers.jpg";
+import orange13 from "../../public/images/catalog-covers/orange/sierra-designs.jpg";
+import orange14 from "../../public/images/catalog-covers/orange/sterling.jpg";
+import orange15 from "../../public/images/catalog-covers/orange/nemo.jpg";
+import yellow1 from "../../public/images/catalog-covers/yellow/alaska.jpg";
+import yellow2 from "../../public/images/catalog-covers/yellow/brooks-co.jpg";
+import yellow3 from "../../public/images/catalog-covers/yellow/camp7.jpg";
+import yellow4 from "../../public/images/catalog-covers/yellow/coleman.jpg";
+import yellow5 from "../../public/images/catalog-covers/yellow/each-year.jpg";
+import yellow6 from "../../public/images/catalog-covers/yellow/eddie-bauer.jpg";
+import yellow7 from "../../public/images/catalog-covers/yellow/frostline.jpg";
+import yellow8 from "../../public/images/catalog-covers/yellow/gramicci.jpg";
+import yellow9 from "../../public/images/catalog-covers/yellow/marmot.jpg";
+import yellow10 from "../../public/images/catalog-covers/yellow/north-face.jpg";
+import yellow11 from "../../public/images/catalog-covers/yellow/patagonia.jpg";
+import yellow12 from "../../public/images/catalog-covers/yellow/portage-pack-co.jpg";
+import yellow13 from "../../public/images/catalog-covers/yellow/sunbird.jpg";
+import yellow14 from "../../public/images/catalog-covers/yellow/surf.jpg";
+import yellow15 from "../../public/images/catalog-covers/yellow/tent-awning-co.jpg";
+import green1 from "../../public/images/catalog-covers/green/abercrombie.jpg";
+import green2 from "../../public/images/catalog-covers/green/alaska.jpg";
+import green3 from "../../public/images/catalog-covers/green/asolo.jpg";
+import green4 from "../../public/images/catalog-covers/green/patagonia.jpg";
+import green5 from "../../public/images/catalog-covers/green/camping2.jpg";
+import green6 from "../../public/images/catalog-covers/green/duckworth.jpg";
+import green7 from "../../public/images/catalog-covers/green/eiger.jpg";
+import green8 from "../../public/images/catalog-covers/green/forrest.jpg";
+import green9 from "../../public/images/catalog-covers/green/frostline.jpg";
+import green10 from "../../public/images/catalog-covers/green/marmot.jpg";
+import green11 from "../../public/images/catalog-covers/green/mtn-hard-wear.jpg";
+import green12 from "../../public/images/catalog-covers/green/patagonia2.jpg";
+import green13 from "../../public/images/catalog-covers/green/camping.jpg";
+import green14 from "../../public/images/catalog-covers/green/reliable-racing-supply.jpg";
+import green15 from "../../public/images/catalog-covers/green/tropical.jpg";
+import blue1 from "../../public/images/catalog-covers/blue/alpenlite.jpg";
+import blue2 from "../../public/images/catalog-covers/blue/black-diamond.jpg";
+import blue3 from "../../public/images/catalog-covers/blue/chouinard.jpg";
+import blue4 from "../../public/images/catalog-covers/blue/cloudveil.jpg";
+import blue5 from "../../public/images/catalog-covers/blue/dps.jpg";
+import blue6 from "../../public/images/catalog-covers/blue/ems.jpg";
+import blue7 from "../../public/images/catalog-covers/blue/frostline.jpg";
+import blue8 from "../../public/images/catalog-covers/blue/marmot.jpg";
+import blue9 from "../../public/images/catalog-covers/blue/gore-tex.jpg";
+import blue10 from "../../public/images/catalog-covers/blue/karhu-titan.jpg";
+import blue11 from "../../public/images/catalog-covers/blue/msr.jpg";
+import blue12 from "../../public/images/catalog-covers/blue/play-hours.jpg";
+import blue13 from "../../public/images/catalog-covers/blue/rei.jpg";
+import blue14 from "../../public/images/catalog-covers/blue/reliable-racing-supply.jpg";
+import blue15 from "../../public/images/catalog-covers/blue/marmot2.jpg";
+import purple1 from "../../public/images/catalog-covers/purple/eureka.jpg";
+import purple2 from "../../public/images/catalog-covers/purple/patagonia.jpg";
+import purple3 from "../../public/images/catalog-covers/purple/black-diamond.jpg";
+import purple4 from "../../public/images/catalog-covers/purple/cascade-kits.jpg";
+import purple5 from "../../public/images/catalog-covers/purple/eagle-river-nordic.jpg";
+import purple6 from "../../public/images/catalog-covers/purple/early-winters.jpg";
+import purple7 from "../../public/images/catalog-covers/purple/five-ten.jpg";
+import purple8 from "../../public/images/catalog-covers/purple/patagonia2.jpg";
+import purple9 from "../../public/images/catalog-covers/purple/eureka2.jpg";
+import purple10 from "../../public/images/catalog-covers/purple/mtn-hardwear.jpg";
+import purple11 from "../../public/images/catalog-covers/purple/peak.jpg";
+import purple12 from "../../public/images/catalog-covers/purple/prana.jpg";
+import purple13 from "../../public/images/catalog-covers/purple/patagonia3.jpg";
+import purple14 from "../../public/images/catalog-covers/purple/the-north-face.jpg";
+import purple15 from "../../public/images/catalog-covers/purple/jagged-edge.jpg";
+import brown1 from "../../public/images/catalog-covers/brown/abercrombie.jpg";
+import brown2 from "../../public/images/catalog-covers/brown/backpackers-pantry.jpg";
+import brown3 from "../../public/images/catalog-covers/brown/black-diamond.jpg";
+import brown4 from "../../public/images/catalog-covers/brown/cloudveil.jpg";
+import brown5 from "../../public/images/catalog-covers/brown/coleman.jpg";
+import brown6 from "../../public/images/catalog-covers/brown/country-ways.jpg";
+import brown7 from "../../public/images/catalog-covers/brown/denali.jpg";
+import brown8 from "../../public/images/catalog-covers/brown/early-winters.jpg";
+import brown9 from "../../public/images/catalog-covers/brown/ems.jpg";
+import brown10 from "../../public/images/catalog-covers/brown/black-diamond2.jpg";
+import brown11 from "../../public/images/catalog-covers/brown/fvi.jpg";
+import brown12 from "../../public/images/catalog-covers/brown/mountain-traders.jpg";
+import brown13 from "../../public/images/catalog-covers/brown/msr.jpg";
+import brown14 from "../../public/images/catalog-covers/brown/sketch.jpg";
+import brown15 from "../../public/images/catalog-covers/brown/ems2.jpg";
+import black1 from "../../public/images/catalog-covers/black/black-diamond.jpg";
+import black2 from "../../public/images/catalog-covers/black/camp-trails.jpg";
+import black3 from "../../public/images/catalog-covers/black/carikit.jpg";
+import black4 from "../../public/images/catalog-covers/black/ems.jpg";
+import black5 from "../../public/images/catalog-covers/black/chouinard.jpg";
+import black6 from "../../public/images/catalog-covers/black/climb-high.jpg";
+import black7 from "../../public/images/catalog-covers/black/dana-design.jpg";
+import black8 from "../../public/images/catalog-covers/black/early-winters.jpg";
+import black9 from "../../public/images/catalog-covers/black/eddie-bauer.jpg";
+import black10 from "../../public/images/catalog-covers/black/epoke.jpg";
+import black11 from "../../public/images/catalog-covers/black/eureka.jpg";
+import black12 from "../../public/images/catalog-covers/black/ems2.jpg";
+import black13 from "../../public/images/catalog-covers/black/forrest.jpg";
+import black14 from "../../public/images/catalog-covers/black/woolrich.jpg";
+import black15 from "../../public/images/catalog-covers/black/yakima.jpg";
+import white1 from "../../public/images/catalog-covers/white/alpine-outfitters.jpg";
+import white2 from "../../public/images/catalog-covers/white/beal.jpg";
+import white3 from "../../public/images/catalog-covers/white/camp7.jpg";
+import white4 from "../../public/images/catalog-covers/white/the-north-face.jpg";
+import white5 from "../../public/images/catalog-covers/white/chouinard.jpg";
+import white6 from "../../public/images/catalog-covers/white/coleman.jpg";
+import white7 from "../../public/images/catalog-covers/white/columbia-sportswear.jpg";
+import white8 from "../../public/images/catalog-covers/white/carabiner.jpg";
+import white9 from "../../public/images/catalog-covers/white/dana-design.jpg";
+import white10 from "../../public/images/catalog-covers/white/dynafit.jpg";
+import white11 from "../../public/images/catalog-covers/white/the-north-face2.jpg";
+import white12 from "../../public/images/catalog-covers/white/early-winters.jpg";
+import white13 from "../../public/images/catalog-covers/white/mountain-tools.jpg";
+import white14 from "../../public/images/catalog-covers/white/norwegian-ski-council.jpg";
+import white15 from "../../public/images/catalog-covers/white/patagonia.jpg";
 
-const catalogCovers = [
-  photo1,
-  photo2,
-  photo3,
-  photo4,
-  photo5,
-  photo6,
-  photo7,
-  photo8,
-  photo9,
-  photo10,
-  photo11,
-  photo12,
-  photo13,
-  photo14,
-  photo15,
-  photo16,
+const redCovers = [
+  red1,
+  red2,
+  red3,
+  red4,
+  red5,
+  red6,
+  red7,
+  red8,
+  red9,
+  red10,
+  red11,
+  red12,
+  red13,
+  red14,
+  red15,
+];
+
+const orangeCovers = [
+  orange1,
+  orange2,
+  orange3,
+  orange4,
+  orange5,
+  orange6,
+  orange7,
+  orange8,
+  orange9,
+  orange10,
+  orange11,
+  orange12,
+  orange13,
+  orange14,
+  orange15,
+];
+
+const yellowCovers = [
+  yellow1,
+  yellow2,
+  yellow3,
+  yellow4,
+  yellow5,
+  yellow6,
+  yellow7,
+  yellow8,
+  yellow9,
+  yellow10,
+  yellow11,
+  yellow12,
+  yellow13,
+  yellow14,
+  yellow15,
+];
+
+const greenCovers = [
+  green1,
+  green2,
+  green3,
+  green4,
+  green5,
+  green6,
+  green7,
+  green8,
+  green9,
+  green10,
+  green11,
+  green12,
+  green13,
+  green14,
+  green15,
+];
+
+const blueCovers = [
+  blue1,
+  blue2,
+  blue3,
+  blue4,
+  blue5,
+  blue6,
+  blue7,
+  blue8,
+  blue9,
+  blue10,
+  blue11,
+  blue12,
+  blue13,
+  blue14,
+  blue15,
+];
+
+const purpleCovers = [
+  purple1,
+  purple2,
+  purple3,
+  purple4,
+  purple5,
+  purple6,
+  purple7,
+  purple8,
+  purple9,
+  purple10,
+  purple11,
+  purple12,
+  purple13,
+  purple14,
+  purple15,
+];
+
+const brownCovers = [
+  brown1,
+  brown2,
+  brown3,
+  brown4,
+  brown5,
+  brown6,
+  brown7,
+  brown8,
+  brown9,
+  brown10,
+  brown11,
+  brown12,
+  brown13,
+  brown14,
+  brown15,
+];
+
+const blackCovers = [
+  black1,
+  black2,
+  black3,
+  black4,
+  black5,
+  black6,
+  black7,
+  black8,
+  black9,
+  black10,
+  black11,
+  black12,
+  black13,
+  black14,
+  black15,
+];
+
+const whiteCovers = [
+  white1,
+  white2,
+  white3,
+  white4,
+  white5,
+  white6,
+  white7,
+  white8,
+  white9,
+  white10,
+  white11,
+  white12,
+  white13,
+  white14,
+  white15,
 ];
 
 export default function HomePage() {
@@ -43,43 +305,39 @@ export default function HomePage() {
     <main className="h-svh max-h-svh lg:grid lg:grid-cols-5 lg:grid-rows-2 overflow-hidden relative">
       <CornerCrosshairs />
       <NameCard />
-      <TopRowCard title="About" linkTo="/about" imageArray={catalogCovers} />
-      <TopRowCard title="Books" linkTo="/books" imageArray={catalogCovers} />
+      <TopRowCard title="About" linkTo="/about" imageArray={redCovers} />
+      <TopRowCard title="Books" linkTo="/books" imageArray={orangeCovers} />
       <TopRowCard
         title="Catalogs"
         linkTo="/catalogs"
-        imageArray={catalogCovers}
+        imageArray={yellowCovers}
       />
       <TopRowCard
         title="Magazines"
         linkTo="/magazines"
-        imageArray={catalogCovers}
+        imageArray={greenCovers}
       />
       <BottomRowCard
         title="Manuscripts"
         linkTo="/manuscripts"
-        imageArray={catalogCovers}
+        imageArray={blueCovers}
       />
       <BottomRowCard
         title="Periodicals"
         linkTo="/periodicals"
-        imageArray={catalogCovers}
+        imageArray={purpleCovers}
       />
       <BottomRowCard
         title="Photographs"
         linkTo="/photographs"
-        imageArray={catalogCovers}
+        imageArray={brownCovers}
       />
       <BottomRowCard
         title="Research Fellowship"
         linkTo="/research-fellowship"
-        imageArray={catalogCovers}
+        imageArray={blackCovers}
       />
-      <BottomRowCard
-        title="Donate"
-        linkTo="/donate"
-        imageArray={catalogCovers}
-      />
+      <BottomRowCard title="Donate" linkTo="/donate" imageArray={whiteCovers} />
     </main>
   );
 }
