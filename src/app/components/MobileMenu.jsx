3aa0f1567e2ff5fdd9b892918d4599869f43397f
gabewolford <gabewolford@gmail.com>
2024-02-18 14:58:12 -0800

    mobile links from gray to black

--- src/app/components/MobileMenu.jsx
+++ src/app/components/MobileMenu.jsx
@@ -47,10 +47,10 @@ export default function MobileMenu() {
 
       {isMobileMenuOpen && (
         <div className="lg:hidden fixed inset-0 bg-white z-[9000] animate__animated animate__slideInDown flex flex-col">
-          <h3 className="flex flex-col w-full p-3 text-gray-main font-medium">
+          <h3 className="flex flex-col w-full p-3 font-medium">
             Outdoor Recreation Archive
           </h3>
-          <div className="flex flex-col flex-1">
+          <div className="flex flex-col flex-1 text-black">
             <Link
               href="/about"
               onClick={toggleMobileMenu}
