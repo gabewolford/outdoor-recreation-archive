ae2749b226ad6007441591f2a80dabb1273c71de
gabewolford <gabewolford@gmail.com>
2024-02-18 13:53:18 -0800

    about page complete

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,187 +1,167 @@
 import Link from "next/link";
+import TheHeader from "../components/TheHeader";
+import ExternalLink from "../components/About/ExternalLink";
 
 export default function AboutPage() {
+  const pressLinks = [
+    {
+      outlet: "Outside Magazine",
+      url: "https://www.outsideonline.com/business-journal/issues/archiving-the-history-of-the-outdoor-industry/",
+    },
+    {
+      outlet: "Gear Patrol",
+      url: "https://www.gearpatrol.com/outdoors/a36412628/utah-state-university-outdoor-recreation-archive/",
+    },
+    {
+      outlet: "Hypebeast",
+      url: "https://hypebeast.com/2020/11/outdoor-recreation-archive-utah-university-interview",
+    },
+    {
+      outlet: "Subukaru Magazine",
+      url: "https://sabukaru.online/articles/outdoor-recreation-archive",
+    },
+    {
+      outlet: "Mountain Gazette",
+      url: "https://mountaingazette.com/blogs/mountain-notes/feature-usu-outdoor-recreation-archive",
+    },
+    {
+      outlet: "Design Reviewed",
+      url: "https://designreviewed.com/the-outdoor-recreation-archive/",
+    },
+    {
+      outlet: "Field Mag",
+      url: "https://www.fieldmag.com/articles/outdoor-recreation-archive-vintage-camp-gear-instagram",
+    },
+  ];
+
+  const socialLinks = [
+    {
+      platform: "Instagram",
+      url: "https://www.instagram.com/outdoorrecarchive/",
+    },
+    {
+      platform: "Threads",
+      url: "https://www.threads.net/@outdoorrecarchive",
+    },
+    {
+      platform: "Facebook",
+      url: "https://www.facebook.com/outdoorrecarchive/",
+    },
+    {
+      platform: "Twitter",
+      url: "https://twitter.com/Outdoor_Archive",
+    },
+  ];
+
   return (
-    <div className="grid grid-cols-4 gap-9 lg:gap-0 lg:grid-cols-12 lg:grid-rows-3 text-sm">
-      <section className="flex flex-col gap-3 col-span-4 lg:col-span-5 lg:row-span-2">
-        <p>
-          In 2017, USU Special Collections and Archives (SCA) partnered with the
-          College of Agriculture and Applied Science's Outdoor Product Design
-          and Development (OPDD) program to develop a historical collection of
-          books, catalogs, periodicals, photographs, and manuscripts documenting
-          the history of outdoor gear. These materials, together with the strong
-          environmental collections SCA has already built since the early 1970s,
-          form the Outdoor Recreation Archive, which is highlighted in this
-          guide.
-        </p>
-        <p>
-          As a public institution, SCA strives to make its collections
-          accessible to anyone, from professional researchers to those with a
-          more casual interest in the outdoor industry. These materials are
-          particularly beneficial to students in the OPDD program, who represent
-          the next generation of leaders in the outdoor industry. The Outdoor
-          Recreation Archive gives these students easy access to historical
-          records from which to better understand the past while drawing
-          inspiration for the future.
-        </p>
-        <p>
-          For instructions on donating to the archive, please visit our{" "}
-          <span>
-            <Link
-              href="/donate"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
+    <main className="p-3 grid grid-cols-4 lg:grid-cols-12 lg:grid-rows-6">
+      <section className="flex flex-col col-span-4 lg:col-span-5 lg:row-span-4 mb-12">
+        <TheHeader title="Introduction" />
+        <div className="flex flex-col gap-3">
+          <p>
+            In 2017, USU Special Collections and Archives (SCA) partnered with
+            the College of Agriculture and Applied Science's Outdoor Product
+            Design and Development (OPDD) program to develop a historical
+            collection of books, catalogs, periodicals, photographs, and
+            manuscripts documenting the history of outdoor gear. These
+            materials, together with the strong environmental collections SCA
+            has already built since the early 1970s, form the Outdoor Recreation
+            Archive, which is highlighted in this guide.
+          </p>
+          <p>
+            As a public institution, SCA strives to make its collections
+            accessible to anyone, from professional researchers to those with a
+            more casual interest in the outdoor industry. These materials are
+            particularly beneficial to students in the OPDD program, who
+            represent the next generation of leaders in the outdoor industry.
+            The Outdoor Recreation Archive gives these students easy access to
+            historical records from which to better understand the past while
+            drawing inspiration for the future.
+          </p>
+          <p>
+            For instructions on donating to the archive, please visit our{" "}
+            <Link href="/donate" className="underline underline-offset-4">
               Donate Page
             </Link>
-          </span>
-          .
-        </p>
-        <p className="hidden lg:block">
-          For information about searching and viewing the archive, please see
-          the information to the right →
-        </p>
-        <p className="lg:hidden">
-          For information about searching and viewing the archive, please see
-          the information to the below ↓
-        </p>
+            .
+          </p>
+          <p className="hidden lg:block">
+            For information about searching and viewing the archive, please see
+            the information to the right →
+          </p>
+          <p className="lg:hidden">
+            For information about searching and viewing the archive, please see
+            the information to the below ↓
+          </p>
+        </div>
       </section>
 
-      <section className="grid grid-cols-4 lg:gap-0 lg:grid-cols-2 lg:col-start-7 col-span-4 lg:col-span-5 lg:row-start-2 lg:row-span-1">
-        <div className="flex flex-col col-span-2 lg:col-span-1 pr-3 lg:p-3">
-          <h3 className="font-medium">Using This Guide</h3>
-          <p className="mb-2">
-            This guide categorizes materials in the archive by format: books,
-            catalogs, magazines, manuscripts, periodicals, and photographs. You
-            can navigate to these different categories via the home page.
-          </p>
-          <p className="mb-2">
-            Additionally, you can find information regarding USU Special
-            Collections and Archives Outdoor Recreation Archive Research
-            Fellowship{" "}
-            <span>
+      <section className="flex flex-row gap-6 col-span-4 lg:col-start-7 lg:col-span-5 lg:row-start-4 lg:row-span-3 text-sm mb-12">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold">Using This Guide</h3>
+            <p>
+              This guide categorizes materials in the archive by format: books,
+              catalogs, magazines, manuscripts, periodicals, and photographs.
+              You can navigate to these different categories via the home page.
+            </p>
+            <p>
+              Additionally, you can find information regarding USU Special
+              Collections and Archives Outdoor Recreation Archive Fellowship{" "}
               <Link
                 href="/research-fellowship"
-                className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
+                className="underline underline-offset-4"
               >
                 here
               </Link>
-            </span>
-            . Applications are accepted September 15 to December 1.
-          </p>
+              . Applications are accepted September 15 to December 1.
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold">Press</h3>
+            <ul className="flex flex-col gap-1">
+              {pressLinks.map((article, index) => (
+                <li key={index}>
+                  <ExternalLink text={article.outlet} linkTo={article.url} />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="flex flex-col col-span-2 lg:col-span-1 pl-3 lg:p-3">
-          <h3 className="font-medium">Viewing The Archive</h3>
-          <p className="mb-2">
-            If you find materials you'd like to view, you can do so by visiting
-            the SCA Reading Room. Plan your visit{" "}
-            <span>
+
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold">Viewing The Archive</h3>
+            <p>
+              If you find materials you'd like to view, you can do so by
+              visiting the SCA Reading Room. Plan your visit{" "}
               <a
                 href="https://library.usu.edu/archives/about/using"
                 target="_blank"
-                className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
+                className="underline underline-offset-4"
               >
                 here
               </a>
               .
-            </span>
-          </p>
-          <p>
-            If you are unable to visit our Reading Room, many materials can be
-            digitized for a fee. The fee schedule can be viewed on the page
-            linked above.
-          </p>
-        </div>
-      </section>
-
-      <section className="grid grid-cols-4 lg:gap-0 lg:grid-cols-2 lg:col-start-7 col-span-4 lg:col-span-5 lg:row-start-3 lg:row-span-1">
-        <div className="flex flex-col col-span-2 lg:col-span-1 pr-3 lg:p-3">
-          <h3 className="font-medium">Press</h3>
-          <div className="flex flex-col gap-0.5">
-            <a
-              href="https://www.outsideonline.com/business-journal/issues/archiving-the-history-of-the-outdoor-industry/"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Outside Magazine
-            </a>
-            <a
-              href="https://www.gearpatrol.com/outdoors/a36412628/utah-state-university-outdoor-recreation-archive/"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Gear Patrol
-            </a>
-            <a
-              href="https://hypebeast.com/2020/11/outdoor-recreation-archive-utah-university-interview"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Hypebeast
-            </a>
-            <a
-              href="https://sabukaru.online/articles/outdoor-recreation-archive"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Subukaru Magazine
-            </a>
-            <a
-              href="https://mountaingazette.com/blogs/mountain-notes/feature-usu-outdoor-recreation-archive"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Mountain Gazette
-            </a>
-            <a
-              href="https://designreviewed.com/the-outdoor-recreation-archive/"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Design Reviewed
-            </a>
-            <a
-              href="https://www.fieldmag.com/articles/outdoor-recreation-archive-vintage-camp-gear-instagram"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Field Mag
-            </a>
+            </p>
+            <p>
+              If you are unable to visit our Reading Room, many materials can be
+              digitized for a fee. The fee schedule can be viewed on the page
+              linked above.
+            </p>
           </div>
-        </div>
-        <div className="flex flex-col col-span-2 lg:col-span-1 pl-3 lg:p-3">
-          <h3 className="font-medium">Follow Us</h3>
-          <div className="flex flex-col gap-0.5">
-            <a
-              href="https://www.instagram.com/outdoorrecarchive/"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Instagram
-            </a>
-            <a
-              href="https://www.threads.net/@outdoorrecarchive"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Threads
-            </a>
-            <a
-              href="https://www.facebook.com/outdoorrecarchive/"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Facebook
-            </a>
-            <a
-              href="https://twitter.com/Outdoor_Archive"
-              target="_blank"
-              className="text-blue-main font-medium hover:text-gray-main hover:underline-offset-4 hover:underline"
-            >
-              Twitter
-            </a>
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold">Press</h3>
+            <ul className="flex flex-col gap-1">
+              {socialLinks.map((social, index) => (
+                <li key={index}>
+                  <ExternalLink text={social.platform} linkTo={social.url} />
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </section>
-    </div>
+    </main>
   );
 }
