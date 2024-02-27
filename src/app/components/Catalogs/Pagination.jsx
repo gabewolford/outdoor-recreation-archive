55247b9b75bddd3385a42e9571a082aa90769840
gabewolford <gabewolford@gmail.com>
2024-02-27 15:45:46 -0800

    created placeholder pagination

---
+++ src/app/components/Catalogs/Pagination.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Pagination({ currentIndex, totalItems }) {
+  const [currentPage, setCurrentPage] = useState(currentIndex);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalItems) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  return (
+    <div className="flex flex-row gap-2">
+      <Link
+        href="/catalogs"
+        onClick={handlePrevPage}
+        className="text-blue-main font-semibold hover:text-gray-subtext"
+      >
+        Previous
+      </Link>
+      {/* <p>{`${currentPage} of ${totalItems}`}</p> */}
+      <p>1 of 3</p>
+      <Link
+        href="/catalogs"
+        onClick={handleNextPage}
+        className="text-blue-main font-semibold hover:text-gray-subtext"
+      >
+        Next
+      </Link>
+    </div>
+  );
+}
