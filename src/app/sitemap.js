f1575639a59ee4974168bca9d7968d8fdeb8bef1
gabewolford <gabewolford@gmail.com>
2024-03-11 12:42:18 -0700

    added sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,64 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://outdoorrecreationarchive.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/catalogs",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/donate",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/research-fellowship",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/books",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.1,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/magazines",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.1,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/manuscripts",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.1,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/periodicals",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.1,
+    },
+    {
+      url: "https://outdoorrecreationarchive.com/photographs",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.1,
+    },
+  ];
+}
